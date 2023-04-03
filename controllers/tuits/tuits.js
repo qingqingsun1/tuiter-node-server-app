a2b44aa5ede3c4bebca0c60a7fc38b42c518a2c2
qingqingsun <sun.qingq@northeastern.edu>
2023-04-02 19:51:30 -0700

    for test

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,15 +1,15 @@
 export default [
   {
     "_id": "123", "topic": "Space", "username": "SpaceX",
-    "handle": "@spacex", "time": "2h", "image": "spacex.png",
+    "handle": "spacex", "time": "2h", "image": "spacex.png",
     "title": "SpaceX's Mission",
     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-    "liked": true, "likes": 2345, "dislikes": 1264,
+    "liked": false, "likes": 2345, "dislikes": 1264,
     "replies": 123, "retuits": 432
   },
   {
     "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-    "handle": "@boringcompany", "time": "2h", "image": "news2.png",
+    "handle": "boringcompany", "time": "2h", "image": "news2.png",
     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
     "liked": true, "likes": 2345, "dislikes": 1264,
@@ -17,10 +17,18 @@ export default [
   },
   {
     "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-    "handle": "@neurolink", "time": "3h", "image": "news3.png",
+    "handle": "neurolink", "time": "3h", "image": "news3.png",
     "title": "Neurolink fixes paralisis and blindness",
     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
     "liked": true, "likes": 3456, "dislikes": 1264,
     "replies": 345, "retuits": 654
   },
+  {
+    "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
+    "handle": "neurolink", "time": "3h", "image": "news3.png",
+    "title": "Neurolink fixes paralisis and blindness",
+    "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+    "liked": true, "likes": 3456, "dislikes": 1264,
+    "replies": 345, "retuits": 654
+  }
   ];
