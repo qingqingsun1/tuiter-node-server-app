39d1b225a61e0ee3bee0f18ebc566a7153e00b6b
qingqingsun <sun.qingq@northeastern.edu>
2023-04-09 17:45:55 -0700

    all

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -2,9 +2,19 @@ import mongoose from 'mongoose';   // load the mongoose library
 
 
 const schema = mongoose.Schema({   // create the schema
-tuit: String,
-likes: Number,
-liked: Boolean,
+    topic: String,
+    username:String,
+    handle:String,
+    time:String,
+    image:String,
+    title:String,
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    likes: Number,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number
 }, 
 {collection: 'tuits'});  // collection name where tuits are stored in tuiter database
 
